63a7b65341344cdcbaecce1214bfd2a7915d6725
niravdinmali <niravdinmali@gmail.com>
2022-04-21 13:58:14 +0530

    using ng content with select attribute

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PaginatorComponent } from './paginator/paginator.component';
 import { BlogDataService } from './blog-data.service';
 import { HighlightDirective } from './highlight.directive';
 import { CardComponent } from './card/card.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CardComponent } from './card/card.component';
     TruncatePipe,
     PaginatorComponent,
     HighlightDirective,
-    CardComponent
+    CardComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule
